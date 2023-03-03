de1b76c4a7131aa691a1de7d4039ae87a9502387
Artem Herych <lawrence.artem@gmail.com>
2023-03-03 20:20:28 +0100

    add pixel class

---
+++ src/pixel.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "pixel.h"
+
+Pixel::Pixel(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0) {
+  this->r = r;
+  this->g = g;
+  this->b = b;
+}
---
+++ src/pixel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Pixel {
+public:
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  Pixel(uint8_t, uint8_t, uint8_t);
+};
